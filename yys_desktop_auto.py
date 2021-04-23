825d53edeacd4ad6a3cd2e5d14aa4cd5442be360
Tim Qin <qinyujue@gmail.com>
2021-04-23 14:27:52 +1000

    Modulize other functions into different filse

--- yys_desktop_auto.py
+++ yys_desktop_auto.py
@@ -1,6 +1,7 @@
 import time, os
 import auto_player as player
 import random
+from modules.friend import _friend_update
 
 def get_pictures():   
     player.screen_shot()
@@ -59,41 +60,6 @@ def _random_sleep(wmin=5,wmax=15):
     sleep_time = random.randint(wmin, wmax)
     print("sleep %d s", sleep_time)
     time.sleep(sleep_time)
-
-
-def _friend_update():
-    count = 0
-    is_waiting = False
-    while count < 10:
-        ar1 = ['friend_receive', 'friend_receive2', 'good_friend_send', 'friend_send','friend_switch', 'friend_panel']
-        re = player.find_touch_any(ar1)
-        if re == 'friend_send':
-            print('发出友情点')
-            count += 1
-            _random_sleep(wmin=1,wmax=1)
-        elif re == 'good_friend_send':
-            print('发出友情点')
-            count += 1
-            _random_sleep(wmin=1,wmax=1)
-        elif re == 'friend_switch':
-            _random_sleep(wmin=1,wmax=1)
-        elif re == 'friend_receive':
-            print('接受要好友情点...')
-            _random_sleep(wmin=1,wmax=1)
-        elif re == 'friend_receive2':
-            print('接受友情点...')
-            _random_sleep(wmin=1,wmax=1)
-        elif re == 'friend_panel':
-            print('进入友情界面...')
-            _random_sleep(wmin=1,wmax=1)
-        print('已经进行了' + str(count) + '次操作。。。')
-    print('友情界面处理完毕，退出....')
-    quick_close_cnt = 0
-    while quick_close_cnt < 2:
-        ar2 = ['close']
-        player.find_touch_any(ar2)
-        quick_close_cnt += 1
-        _random_sleep(wmin=1,wmax=1)
 
 
 def daily_job():
